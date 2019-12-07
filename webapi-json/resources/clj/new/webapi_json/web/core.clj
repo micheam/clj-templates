b433f03eb866664404b4f7e37908c57dd650ee63
Michito Maeda <michito.maeda@gmail.com>
2019-12-07 19:15:37 +0900

    Set port from environment variable
    Fix #1

--- webapi-json/resources/clj/new/webapi_json/web/core.clj
+++ webapi-json/resources/clj/new/webapi_json/web/core.clj
@@ -1,15 +1,15 @@
-(ns {{namespace}}.web.core
-  (:require [ring.adapter.jetty :as s]
-            [ring.middleware.json :refer [wrap-json-response wrap-json-params]]
-            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.middleware.params :refer [wrap-params]]
-            [clojure.tools.logging :as log]
-            [{{namespace}}.web.route :refer [{{name}}-routes]]))
+(ns {{namespace}} .web.core
+    (:require [ring.adapter.jetty :as s]
+              [ring.middleware.json :refer [wrap-json-response wrap-json-params]]
+              [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+              [ring.middleware.params :refer [wrap-params]]
+              [clojure.tools.logging :as log]
+              [{{namespace}} .web.route :refer [{{name}} -routes]]))
 
 (defonce server (atom nil))
 
 (def app
-  (-> {{name}}-routes
+  (-> {{name}} -routes
       wrap-keyword-params
       wrap-json-params
       wrap-json-response
@@ -17,9 +17,11 @@
 
 (defn start []
   (when-not @server
-    (let [port 8080]
-    (reset! server (s/run-jetty #'app {:port port :join? false}))
-    (log/infof "server started on port %d" port))))
+    (let [port (if-let [p (System/getenv "PORT")]
+                 (Integer/parseInt p)
+                 8080)]
+      (reset! server (s/run-jetty #'app {:port port :join? false}))
+      (log/infof "server started on port %d" port))))
 
 (defn stop []
   (when @server
